59b6320228525ec00c1a6f2c861ffd6fd5a0b68c
Miguel-Dorta <contact@migueldorta.com>
2017-07-12 23:07:30 +0100

    Added (part of) locations, bug fixed & part of partitioning code

--- easylinux.sh
+++ easylinux.sh
@@ -7,6 +7,7 @@ timeZone="/usr/share/zoneinfo/Etc/UTC"
 isUEFImode=false
 isConnectedToInternet=false
 declare -a additionalLanguages
+declare -i counter=0
 
 
 # Previous checking of internet connection
@@ -55,7 +56,7 @@ read -p "Choose your region from the list above: " region
 if [ $region = "America" ]; then
 	(cd /usr/share/zoneinfo/America && ls -d */)
 	read -p "Is your region one of those above? If it's yes, introduce it. If it's not, press ENTER: " subregion
-	if [ subregion != "" ]; then
+	if [[ $subregion ]]; then
 		region=$region'/'$subregion
 	fi
 fi
@@ -79,10 +80,32 @@ timedatectl set-ntp true
 ###########################################
 # Partitioning and formatting script here #
 ###########################################
+#
+### UEFI clean disk installation
+# # Make partition table
+# parted <disk> mklabel gpt
+#
+# # Make EFI partition
+# parted <disk> mkpart ESP fat32 1MiB 513MiB
+# parted <disk> set 1 boot on
+# mkfs.fat -F32 <disk/partition1>
+# 
+# # Make system partition
+# parted <disk> mkpart primary xfs 513MiB 100%
+# mkfs.xfs <disk/partition2>
+# 
+# # Mount partitions
+# mount <disk/partition2> /mnt
+# mkdir /mnt/boot
+# mount <disk/partition1> /mnt/boot
+# 
+###########################################
+# Partitioning and formatting script here #
+###########################################
 
 
 # Sorting mirrors by their speed
-# THIS NEEDS TO BE CHANGED. RANKMIRRORS WILL BE UNAVAILABLE IN THE RELEASE OF PACMAN
+# THIS NEEDS TO BE CHANGED. RANKMIRRORS WILL BE UNAVAILABLE IN THE NEXT RELEASE OF PACMAN
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
 rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
@@ -93,8 +116,15 @@ pacstrap /mnt base
 # Generate fstab file
 genfstab -U /mnt >> /mnt/etc/fstab
 
+# Adding locations
+while [[ ${additionalLanguages[$counter]} ]]; do
+	echo "${additionalLanguages[$counter]}.UTF-8 UTF-8" >> /mnt/etc/locale.gen
+	counter+=1
+done
+
 # Operating now into the new system
 arch-chroot /mnt << EOF
 	ln -sf $timeZone /etc/localtime
 	hwclock --systohc
+	locale-gen
 EOF
