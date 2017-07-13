635314268fa6b80848c084f08c66721f44a12e21
Miguel-Dorta <contact@migueldorta.com>
2017-07-13 22:59:32 +0100

    Added partitioning for a clean installation

--- easylinux.sh
+++ easylinux.sh
@@ -5,9 +5,15 @@ language="en_US"
 keyboardLayout="us"
 timeZone="/usr/share/zoneinfo/Etc/UTC"
 localHostname="arch"
+installationDisk="/dev/sda"
+otherDisk="/dev/sdb"
 isUEFImode=false
 isConnectedToInternet=false
+yn1="n"
+yn2="n"
 declare -a additionalLanguages
+declare -i installationMode=0
+declare -i installationOption=0
 declare -i counter=0
 
 
@@ -67,6 +73,34 @@ timeZone="/usr/share/zoneinfo/$region/$city"
 # Set hostname
 read -p "Define system's hostname: " localHostname
 
+# Set installation mode
+printf "\n1/Clean    2/Dual boot (Windows)    3/Dual boot (Mac)\n"
+read -p "Choose one of the installation methods from the list above: " installationMode
+while [ $installationMode < 1 -o $installationMode > 3 ]; do
+	read -p "Please, write the number of the installation methods above: " installationMode
+done
+if [ $installationMode != 1 ]; then
+	printf "\n1/Both OS in the same disk    2/Each OS in a different disk\n"
+	read -p "Choose one of the options: " installationOption
+	while [ $installationOption < 1 -o $installationOption > 2 ]; do
+		read -p "Please, write the number of the options above: " installationOption
+	done
+fi
+if [ $installationOption = 2 ]; then
+	read -p "Do you want to use a boot manager in common? [y/n]: " yn2
+	while [ $yn2 != "y" -a $yn2 != "n" ]; do
+		read -p "Please, write \"y\" or \"n\" (without quotes): " yn2
+	done
+fi
+echo " "
+parted -l
+read -p "Select the disk you want to install ArchLinux: " installationDisk
+if [ $yn2 = "y" ]; then
+	read -p "Select the disk where the other OS is installed: " otherDisk
+fi
+part1=$installationDisk+1
+part2=$installationDisk+2
+
 
 ### Installing process ###
 # Check boot mode
@@ -79,33 +113,35 @@ fi
 # Update system clock
 timedatectl set-ntp true
 
+# Partitioning and formatting
+if [ $installationMode = 1 ]; then
+	if [ $isUEFImode = true ]; then
+		# Make partition table
+		parted $installationDisk mklabel gpt
 
-###########################################
-# Partitioning and formatting script here #
-###########################################
-#
-### UEFI clean disk installation
-# # Make partition table
-# parted <disk> mklabel gpt
-#
-# # Make EFI partition
-# parted <disk> mkpart ESP fat32 1MiB 513MiB
-# parted <disk> set 1 boot on
-# mkfs.fat -F32 <disk/partition1>
-# 
-# # Make system partition
-# parted <disk> mkpart primary xfs 513MiB 100%
-# mkfs.xfs <disk/partition2>
-# 
-# # Mount partitions
-# mount <disk/partition2> /mnt
-# mkdir /mnt/boot
-# mount <disk/partition1> /mnt/boot
-# 
-###########################################
-# Partitioning and formatting script here #
-###########################################
+		# Make EFI partition
+		parted $installationDisk mkpart ESP fat32 1MiB 513MiB
+		parted $installationDisk set 1 boot on
+		mkfs.fat -F32 $part1
+	else
+		# Make partition table
+		parted $installationDisk mklabel msdos
+		
+		# Make boot partition
+		parted $installationDisk mkpart primary ext4 1MiB 513MiB
+		parted $installationDisk set 1 boot on
+		mkfs.ext4 $part1
+		tune2fs -O ^has_journal $part1
+	fi
+	# Make system partition
+	parted $installationDisk mkpart primary xfs 513MiB 100%
+	mkfs.xfs $part2
 
+	# Mount partitions
+	mount $part2 /mnt
+	mkdir /mnt/boot
+	mount $part1 /mnt/boot
+fi
 
 # Sorting mirrors by their speed
 # THIS NEEDS TO BE CHANGED. RANKMIRRORS WILL BE UNAVAILABLE IN THE NEXT RELEASE OF PACMAN
@@ -129,7 +165,7 @@ done
 # Operating now into the new system
 arch-chroot /mnt << EOF
 
-	# dding time zone
+	# Adding time zone
 	ln -sf $timeZone /etc/localtime
 	hwclock --systohc
 
